24e0db093592c3d37b4fb805937252c5010bb0a0
Andressamarcucci <andressamarcucci45@gmail.com>
2025-06-12 23:46:20 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["placehold.co"],
+  },
+  // Remove swcMinify or update to newer syntax if needed
+}
+
+module.exports = nextConfig
